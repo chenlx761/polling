30b70766f9682f1121b3a610b102736184d3bb19
quanming.du <duquanming@kirisun.com>
2026-05-13 16:56:34 +0800

    1.更新版本号

--- app/src/main/java/com/zhuowei/polling/constants/HttpConstants.java
+++ app/src/main/java/com/zhuowei/polling/constants/HttpConstants.java
@@ -4,8 +4,8 @@ public class HttpConstants {
 
     public static String HD_BASE_URL = "";//虎盾的baseURl
 
-//    public static final String BASE_HOST = "szbl-api.dev.tw.com";
-    public static final String BASE_HOST = "172.22.38.13:8080";
+    public static final String BASE_HOST = "szbl-api.dev.tw.com";
+//    public static final String BASE_HOST = "172.22.38.13:8080";
     public static final String BASE_URL = "http://" + BASE_HOST;
 
     public static final String GET_TS_ID_URL = "appLogin";//登录1
